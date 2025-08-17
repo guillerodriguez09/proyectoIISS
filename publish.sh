4fac7aca5257d4878f9d3b42d8020a421ede16a5
guillerodriguez09 <rodriguezguillermina816@gmail.com>
2025-08-17 20:51:58 -0300

    agrego comandos y moore

---
+++ publish.sh
@@ -0,0 +1,22 @@
+HOST=${HOST:-localhost}
+PORT=${PORT:-15000}
+
+
+#publican mensajes 
+mosquitto_pub -h "$HOST" -p "$PORT" -t "shelly/ht" -m '{"temp":23.7,"hum":61}'
+mosquitto_pub -h "$HOST" -p "$PORT" -t "shelly/ht" -m '{"temp":25.0,"hum":58}'
+mosquitto_pub -h "$HOST" -p "$PORT" -t "shelly/ht" -m '{"temp":24.1,"hum":60}'
+
+mosquitto_pub -h "$HOST" -p "$PORT" -t "shelly/switch" -m '{"state":"ON","power":42.5}'
+mosquitto_pub -h "$HOST" -p "$PORT" -t "shelly/switch" -m '{"state":"ON","power":65.2}' 
+mosquitto_pub -h "$HOST" -p "$PORT" -t "shelly/switch" -m '{"state":"OFF","power":0.0}'
+
+#consumen mensajes
+#escucha todos los de shelly
+mosquitto_sub -h localhost -p 15000 -t 'shelly/#' -v
+#escucha los de shelly/ht
+mosquitto_sub -h localhost -p 15000 -t 'shelly/ht' -v
+#escucha los de shelly/switch
+mosquitto_sub -h localhost -p 15000 -t 'shelly/switch' -v
+
+
